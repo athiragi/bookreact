2e6a03fe454d6b173096b6b177fc428425e686fd
athiragi <athirasnair72476@gmail.com>
2022-05-17 12:07:34 +0530

    addbook

--- src/component/Addbook.js
+++ src/component/Addbook.js
@@ -12,11 +12,11 @@ const Addbook = () => {
                     <input type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label for="" className="form-label">Book Title</label>
+                    <label for="" className="form-label">Author</label>
                     <input type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                       <label for="" className="form-label">Book Title</label>
+                       <label for="" className="form-label">Price</label>
                     <input type="text" className="form-control" />
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
