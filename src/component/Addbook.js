16fe59bbfdbb1a76c240a662fcd914294e6933ba
athiragi <athirasnair72476@gmail.com>
2022-05-17 12:15:13 +0530

    booksearch

--- src/component/Addbook.js
+++ src/component/Addbook.js
@@ -4,7 +4,7 @@ const Addbook = () => {
   return (
     <div>
         <div className="container">
-    <div className="row">
+    <div className="row g-3">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
